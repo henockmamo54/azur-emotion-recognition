20709138ae0564082bac6d50692e366d7c94f487
DESKTOP-IUH4BPV\Henock <henockmamo54@gmail.com>
2017-10-03 12:21:57 +0900

    project created

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,347 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.imageEmotion_filepath = new System.Windows.Forms.TextBox();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txt_surprise = new System.Windows.Forms.Label();
+            this.txt_sadness = new System.Windows.Forms.Label();
+            this.label16 = new System.Windows.Forms.Label();
+            this.label17 = new System.Windows.Forms.Label();
+            this.txt_neutral = new System.Windows.Forms.Label();
+            this.txt_happiness = new System.Windows.Forms.Label();
+            this.txt_fear = new System.Windows.Forms.Label();
+            this.txt_disgust = new System.Windows.Forms.Label();
+            this.txt_contempt = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txt_anger = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(511, 16);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Browse";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(12, 13);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(725, 497);
+            this.tabControl1.TabIndex = 1;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.tabPage1.Controls.Add(this.groupBox1);
+            this.tabPage1.Controls.Add(this.pictureBox1);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.imageEmotion_filepath);
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(717, 471);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Emotion";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(26, 44);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(461, 361);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "File Path";
+            // 
+            // imageEmotion_filepath
+            // 
+            this.imageEmotion_filepath.Location = new System.Drawing.Point(87, 18);
+            this.imageEmotion_filepath.Name = "imageEmotion_filepath";
+            this.imageEmotion_filepath.Size = new System.Drawing.Size(400, 20);
+            this.imageEmotion_filepath.TabIndex = 1;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(717, 471);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txt_surprise);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txt_sadness);
+            this.groupBox1.Controls.Add(this.txt_anger);
+            this.groupBox1.Controls.Add(this.label16);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label17);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.txt_neutral);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.txt_happiness);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.txt_fear);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.txt_disgust);
+            this.groupBox1.Controls.Add(this.txt_contempt);
+            this.groupBox1.Location = new System.Drawing.Point(511, 54);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 351);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Score";
+            // 
+            // txt_surprise
+            // 
+            this.txt_surprise.AutoSize = true;
+            this.txt_surprise.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_surprise.Location = new System.Drawing.Point(79, 249);
+            this.txt_surprise.Name = "txt_surprise";
+            this.txt_surprise.Size = new System.Drawing.Size(0, 13);
+            this.txt_surprise.TabIndex = 35;
+            // 
+            // txt_sadness
+            // 
+            this.txt_sadness.AutoSize = true;
+            this.txt_sadness.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_sadness.Location = new System.Drawing.Point(79, 215);
+            this.txt_sadness.Name = "txt_sadness";
+            this.txt_sadness.Size = new System.Drawing.Size(0, 13);
+            this.txt_sadness.TabIndex = 34;
+            // 
+            // label16
+            // 
+            this.label16.AutoSize = true;
+            this.label16.Location = new System.Drawing.Point(15, 249);
+            this.label16.Name = "label16";
+            this.label16.Size = new System.Drawing.Size(45, 13);
+            this.label16.TabIndex = 33;
+            this.label16.Text = "Surprise";
+            // 
+            // label17
+            // 
+            this.label17.AutoSize = true;
+            this.label17.Location = new System.Drawing.Point(15, 215);
+            this.label17.Name = "label17";
+            this.label17.Size = new System.Drawing.Size(48, 13);
+            this.label17.TabIndex = 32;
+            this.label17.Text = "Sadness";
+            // 
+            // txt_neutral
+            // 
+            this.txt_neutral.AutoSize = true;
+            this.txt_neutral.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_neutral.Location = new System.Drawing.Point(79, 192);
+            this.txt_neutral.Name = "txt_neutral";
+            this.txt_neutral.Size = new System.Drawing.Size(0, 13);
+            this.txt_neutral.TabIndex = 31;
+            // 
+            // txt_happiness
+            // 
+            this.txt_happiness.AutoSize = true;
+            this.txt_happiness.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_happiness.Location = new System.Drawing.Point(79, 167);
+            this.txt_happiness.Name = "txt_happiness";
+            this.txt_happiness.Size = new System.Drawing.Size(0, 13);
+            this.txt_happiness.TabIndex = 30;
+            // 
+            // txt_fear
+            // 
+            this.txt_fear.AutoSize = true;
+            this.txt_fear.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_fear.Location = new System.Drawing.Point(79, 135);
+            this.txt_fear.Name = "txt_fear";
+            this.txt_fear.Size = new System.Drawing.Size(0, 13);
+            this.txt_fear.TabIndex = 29;
+            // 
+            // txt_disgust
+            // 
+            this.txt_disgust.AutoSize = true;
+            this.txt_disgust.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_disgust.Location = new System.Drawing.Point(79, 98);
+            this.txt_disgust.Name = "txt_disgust";
+            this.txt_disgust.Size = new System.Drawing.Size(0, 13);
+            this.txt_disgust.TabIndex = 28;
+            // 
+            // txt_contempt
+            // 
+            this.txt_contempt.AutoSize = true;
+            this.txt_contempt.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_contempt.Location = new System.Drawing.Point(79, 66);
+            this.txt_contempt.Name = "txt_contempt";
+            this.txt_contempt.Size = new System.Drawing.Size(0, 13);
+            this.txt_contempt.TabIndex = 27;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(15, 192);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(41, 13);
+            this.label8.TabIndex = 26;
+            this.label8.Text = "Neutral";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(15, 167);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(57, 13);
+            this.label7.TabIndex = 25;
+            this.label7.Text = "Happiness";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(15, 135);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(28, 13);
+            this.label6.TabIndex = 24;
+            this.label6.Text = "Fear";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(15, 98);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(42, 13);
+            this.label5.TabIndex = 23;
+            this.label5.Text = "Disgust";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(15, 66);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(52, 13);
+            this.label4.TabIndex = 22;
+            this.label4.Text = "Contempt";
+            // 
+            // txt_anger
+            // 
+            this.txt_anger.AutoSize = true;
+            this.txt_anger.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_anger.Location = new System.Drawing.Point(79, 34);
+            this.txt_anger.Name = "txt_anger";
+            this.txt_anger.Size = new System.Drawing.Size(0, 13);
+            this.txt_anger.TabIndex = 21;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 34);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 20;
+            this.label2.Text = "Anger";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(749, 522);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox imageEmotion_filepath;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label txt_surprise;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label txt_sadness;
+        private System.Windows.Forms.Label txt_anger;
+        private System.Windows.Forms.Label label16;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label17;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label txt_neutral;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label txt_happiness;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label txt_fear;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label txt_disgust;
+        private System.Windows.Forms.Label txt_contempt;
+    }
+}
+
---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,133 @@
+﻿using System.Windows.Forms;
+using System;
+using System.IO;
+using System.Net.Http.Headers;
+using System.Net.Http;
+using System.Drawing;
+using WindowsFormsApplication1.Models;
+using Newtonsoft.Json;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        OpenFileDialog openFileDialog1;
+        RootObject resoponseImageEmotion;
+        float scalefactor_w = 1;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            openFileDialog1 = new OpenFileDialog();
+        }
+
+        byte[] GetImageAsByteArray(string imageFilePath)
+        {
+            FileStream fileStream = new FileStream(imageFilePath, FileMode.Open, FileAccess.Read);
+            BinaryReader binaryReader = new BinaryReader(fileStream);
+            return binaryReader.ReadBytes((int)fileStream.Length);
+        }
+
+        async void MakeRequest(string imageFilePath)
+        {
+            var client = new HttpClient();
+
+            // Request headers - replace this example key with your valid key.
+            client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", "99126fd28ac54031bc0f0331c706225f");
+
+            // NOTE: You must use the same region in your REST call as you used to obtain your subscription keys.
+            //   For example, if you obtained your subscription keys from westcentralus, replace "westus" in the 
+            //   URI below with "westcentralus".
+            string uri = "https://westus.api.cognitive.microsoft.com/emotion/v1.0/recognize?";
+            HttpResponseMessage response;
+            string responseContent;
+
+            // Request body. Try this sample with a locally stored JPEG image.
+            byte[] byteData = GetImageAsByteArray(imageFilePath);
+
+            using (var content = new ByteArrayContent(byteData))
+            {
+                // This example uses content type "application/octet-stream".
+                // The other content types you can use are "application/json" and "multipart/form-data".
+                content.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
+                response = await client.PostAsync(uri, content);
+                responseContent = response.Content.ReadAsStringAsync().Result;
+
+
+                resoponseImageEmotion = JsonConvert.DeserializeObject<RootObject>(responseContent.Replace("[", "").Replace("]", ""));
+
+                if (resoponseImageEmotion != null)
+                {
+                    txt_anger.Text = ": " + Math.Round(resoponseImageEmotion.scores.anger * 100, 2);
+                    txt_contempt.Text = ": " + Math.Round(resoponseImageEmotion.scores.contempt * 100, 2);
+                    txt_disgust.Text = ": " + Math.Round(resoponseImageEmotion.scores.disgust * 100, 2);
+                    txt_fear.Text = ": " + Math.Round(resoponseImageEmotion.scores.fear * 100, 2);
+                    txt_happiness.Text = ": " + Math.Round(resoponseImageEmotion.scores.happiness * 100, 2);
+                    txt_neutral.Text = ": " + Math.Round(resoponseImageEmotion.scores.neutral * 100, 2);
+                    txt_sadness.Text = ": " + Math.Round(resoponseImageEmotion.scores.sadness * 100, 2);
+                    txt_surprise.Text = ": " + Math.Round(resoponseImageEmotion.scores.surprise * 100, 2);
+                }
+                pictureBox1.Refresh();
+
+            }
+
+            //A peak at the JSON response.
+            Console.WriteLine(responseContent);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DialogResult result = openFileDialog1.ShowDialog();
+            if (result == DialogResult.OK) // Test result.
+            {
+                resoponseImageEmotion = null;
+                Console.WriteLine(openFileDialog1.FileName);
+
+                string imageFilePath = openFileDialog1.FileName;
+                imageEmotion_filepath.Text = imageFilePath;
+                MakeRequest(imageFilePath);
+
+                pictureBox1.Image = Image.FromFile(imageFilePath);
+                this.pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+
+                //pictureBox1.Refresh();
+            }
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            if (resoponseImageEmotion != null)
+            {
+                Graphics g = e.Graphics;
+
+
+                scalefactor_w = pictureBox1.Image.Width / (float)pictureBox1.Width;
+                if (scalefactor_w < 1)
+                {
+                    scalefactor_w = pictureBox1.Width / (float)pictureBox1.Image.Width;
+                    float scalefactor_h = pictureBox1.Height / (float)pictureBox1.Image.Height;
+
+                    Rectangle rect = new Rectangle((int)(resoponseImageEmotion.faceRectangle.left * scalefactor_w), (int)(resoponseImageEmotion.faceRectangle.top * scalefactor_h),
+                    (int)(resoponseImageEmotion.faceRectangle.width * scalefactor_w), (int)(resoponseImageEmotion.faceRectangle.height * scalefactor_h));
+
+                    g.DrawRectangle(System.Drawing.Pens.Red, rect);
+                }
+                else
+                {
+
+                    float scalefactor_h = pictureBox1.Image.Height / (float)pictureBox1.Height;
+
+                    Rectangle rect = new Rectangle((int)(resoponseImageEmotion.faceRectangle.left / scalefactor_w), (int)(resoponseImageEmotion.faceRectangle.top / scalefactor_h),
+                    (int)(resoponseImageEmotion.faceRectangle.width / scalefactor_w), (int)(resoponseImageEmotion.faceRectangle.height / scalefactor_h));
+
+                    g.DrawRectangle(System.Drawing.Pens.Red, rect);
+                }
+            }
+        }
+    }
+}
